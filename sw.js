0b8862eb9d522afe4907cc858016188b5f848595
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-04-12 22:09:21 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+/**
+ * 0o - Service Worker
+ * Purpose: Cache-Lock & Offline Persistence
+ */
+
+const CACHE_NAME = '0o-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './manifest.json',
+    './assets/icon.png',
+    './assets/icon192.png',
+    './modules/kernel.js',
+    './modules/ppu.js',
+    './modules/audio.js',
+    './modules/clock.js',
+    './modules/rdram.js',
+    './modules/linker.js',
+    './src/manager.js',
+    './src/grid.js',
+    './src/canvas.js',
+    './src/renderer.js',
+    './src/fileconverter.js',
+    './src/base.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
